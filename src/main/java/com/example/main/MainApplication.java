f8624da2dc9afc58163811b21872148117cdbb3e
tchen <tchen@murex.com>
2022-01-21 14:25:44 +0100

    Ãbreak a test

--- src/main/java/com/example/main/MainApplication.java
+++ src/main/java/com/example/main/MainApplication.java
@@ -2,6 +2,7 @@ package com.example.main;
 
 public class MainApplication {
 
+    // For testing
     public static void main(String[] args) {
         System.out.println("Start application.");
     }
